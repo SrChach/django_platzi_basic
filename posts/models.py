180262b9e24af15b0f490bcbf0412ef176c3340f
srchach <gsikmo@gmail.com>
2020-06-11 18:29:13 -0500

    FEATURE: Playing with data and connection to databases. Starting with models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,17 @@
+"""Posts models."""
+
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    """User model just for DB learning purposes."""
+
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    bio = models.TextField(blank=True)
+    birthdate = models.DateField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
