878d82fe3a7e8cb09be7c50c6bbca9f421b06c3e
srchach <gsikmo@gmail.com>
2020-06-15 15:27:53 -0500

    FEATURE: Adding custom user to Admin. Filtering, searching and displaying into admin view

--- users/admin.py
+++ users/admin.py
@@ -12,4 +12,21 @@ from users.models import Profile
 class ProfileAdmin(admin.ModelAdmin):
     """Profile admin."""
 
-    list_display = ('user', 'phone_number', 'website')
+    list_display = ('pk', 'user', 'phone_number', 'website', 'picture')
+    list_display_links = ('user',)
+    list_editable = ('phone_number', 'website', 'picture')
+
+    search_fields = (
+        'user__username',
+        'user__email',
+        'user__first_name',
+        'user__last_name',
+        'phone_number'
+    )
+
+    list_filter = (
+        'user__is_active',
+        'user__is_staff',
+        'created',
+        'modified'
+    )
