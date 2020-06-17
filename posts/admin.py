4bbec912792a7a14542da71585143070f9049e78
srchach <gsikmo@gmail.com>
2020-06-17 16:51:25 -0500

    FEATURE: Adding Posts to Django Admin.

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,28 @@
+"""Post admin classes."""
+
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# Posts model
+from posts.models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Post in Admin view."""
+
+    list_display = ('user', 'title', 'photo')
+    list_display_links = ('title',)
+    list_editable = ('photo',)
+
+    search_fields = (
+        'user__username',
+        'user__first_name',
+        'user__last_name',
+        'title'
+    )
+
+    list_filter = (
+        'created',
+        'modified',
+        'user__username'
+    )
