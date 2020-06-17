9efbdcd80104dcd305ef6b6039787bfc21e20578
srchach <gsikmo@gmail.com>
2020-06-17 17:34:48 -0500

    FEATURE: Serving templates and staticfiles. Using templates. Changed STATIC directory

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,43 @@
 """Application views"""
 
-from django.http import HttpResponse
+# Utilities
+from datetime import datetime
 
-def list_posts (request):
-    """List existing posts"""
-    return HttpResponse('Hello from new app')
+# Django
+from django.shortcuts import render
+
+
+POSTS = [
+    {
+        'title': 'Mont Blanc',
+        'user': {
+            'name': 'Yésica Cortés',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/600?image=1036',
+    },
+    {
+        'title': 'Via Láctea',
+        'user': {
+            'name': 'Christian Van der Henst',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/800/?image=903',
+    },
+    {
+        'title': 'Nuevo auditorio',
+        'user': {
+            'name': 'Uriel (thespianartist)',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/500/700/?image=1076',
+    }
+]
+
+
+def list_posts(request):
+    """List existing posts."""
+    return render(request, 'posts/feed.html', {'posts': POSTS})
